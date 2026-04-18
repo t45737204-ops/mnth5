16867bf461e9e9724163b0af095b6a60c4e9d4d7
t45737204-ops <t45737204@gmail.com>
2026-04-18 18:12:04 +0600

    dz1

---
+++ DZ_1_KAZATOV_TURAN/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+ function Header() {
+ 
+const headerFn = ()=> {
+  console.log("Header");
+  
+}
+  return (
+    <div className='container'>
+      <div className='header'>
+        <h1>Welcome</h1>
+        <button 
+          className='btn'
+          onClick={headerFn()}
+        >Нажать</button>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
